e78e76ce3dbc12d412a1be57affdea3e5fb4d135
Gopal Pattanaik <gopalkrushna.pattanaik@tesco.com>
2018-07-31 14:05:21 +0530

    added comments

--- Algorithms/src/com/practice/algorithm/array/SeggregateZerosOnesTwos.java
+++ Algorithms/src/com/practice/algorithm/array/SeggregateZerosOnesTwos.java
@@ -2,6 +2,25 @@ package com.practice.algorithm.array;
 
 import java.util.Scanner;
 
+/**
+ * @author fq63
+ * 
+ *         Sort an array of 0s, 1s and 2s Given an array A[] consisting 0s, 1s
+ *         and 2s, write a function that sorts A[]. The functions should put all
+ *         0s first, then all 1s and all 2s in last.
+ * 
+ * 
+ *         Examples:
+ * 
+ *         Input : {0, 1, 2, 0, 1, 2} 
+ *         
+ *         Output : {0, 0, 1, 1, 2, 2}
+ * 
+ *         Input : {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1}
+ *         
+ *         Output : {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2}
+ *
+ */
 public class SeggregateZerosOnesTwos {
 
 	public static void main(String[] args) {
@@ -41,9 +60,9 @@ public class SeggregateZerosOnesTwos {
 				mid++;
 				break;
 			case 2:
-				int temp1= arr[end];
-				arr[end]=arr[mid];
-				arr[mid]=temp1;
+				int temp1 = arr[end];
+				arr[end] = arr[mid];
+				arr[mid] = temp1;
 				end--;
 				break;
 			}
