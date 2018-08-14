17949afa0692ca52c3933ebf16b78b270816ae00
Gopal Pattanaik <gopalkrushna.pattanaik@tesco.com>
2018-08-14 15:38:59 +0530

    refactoed

--- Algorithms/src/com/practice/algorithm/linkedlist/GetNodeValue.java
+++ Algorithms/src/com/practice/algorithm/linkedlist/GetNodeValue.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 import com.practice.algorithm.linkedlist.mergeSortedLinkedLists.SinglyLinkedList;
 import com.practice.algorithm.linkedlist.mergeSortedLinkedLists.SinglyLinkedListNode;
 
+/**
+ * @author fq63
+ * 
+ *         You’re given the pointer to the head node of a linked list and a
+ *         specific position. Counting backwards from the tail node of the
+ *         linked list, get the value of the node at the given position. A
+ *         position of 0 corresponds to the tail, 1 corresponds to the node
+ *         before the tail and so on.
+ *         
+ *
+ */
 public class GetNodeValue {
 
 	public static void main(String[] args) {
