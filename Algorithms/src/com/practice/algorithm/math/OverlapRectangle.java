f589fa0584aca4bf6d59edb6f749b62a11a5626d
gopalkrushnapattanaik <gopalkrushnapattanaik@gmail.com>
2025-05-31 20:52:37 +0530

    Rectangle Problems

---
+++ Algorithms/src/com/practice/algorithm/math/OverlapRectangle.java
@@ -0,0 +1,61 @@
+package src.com.practice.algorithm.math;
+
+/*
+An axis-aligned rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) is the coordinate of its bottom-left corner, and (x2, y2) is the coordinate of its top-right corner. Its top and bottom edges are parallel to the X-axis, and its left and right edges are parallel to the Y-axis.
+
+Two rectangles overlap if the area of their intersection is positive. To be clear, two rectangles that only touch at the corner or edges do not overlap.
+
+Given two axis-aligned rectangles rec1 and rec2, return true if they overlap, otherwise return false.
+
+
+
+Example 1:
+
+Input: rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+Output: true
+Example 2:
+
+Input: rec1 = [0,0,1,1], rec2 = [1,0,2,1]
+Output: false
+Example 3:
+
+Input: rec1 = [0,0,1,1], rec2 = [2,2,3,3]
+Output: false
+
+
+Constraints:
+
+rec1.length == 4
+rec2.length == 4
+-109 <= rec1[i], rec2[i] <= 109
+rec1 and rec2 represent a valid rectangle with a non-zero area.
+* */
+public class OverlapRectangle {
+
+    public static void main(String[] args) {
+        OverlapRectangle obj = new OverlapRectangle();
+        int[] rec1 = {0, 0, 1, 1};
+        int[] rec2 = {1, 0, 2, 1};
+        System.out.println("isRectangleOverlap :: " + obj.isRectangleOverlap(rec1, rec2));
+    }
+
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        boolean result = true;
+        //take input points of rec1
+        int r1x1 = rec1[0];
+        int r1y1 = rec1[1];
+        int r1x2 = rec1[2];
+        int r1y2 = rec1[3];
+        //take input points of rec2
+        int r2x1 = rec2[0];
+        int r2y1 = rec2[1];
+        int r2x2 = rec2[2];
+        int r2y2 = rec2[3];
+
+        //the rectangle is to the right or left or top bottom
+        if (r1x2 <= r2x1 || r2x2 <= r1x1 || r1y2 <= r2y1 || r2y2 <= r1y1) {
+            return false;
+        }
+        return result;
+    }
+}
---
+++ Algorithms/src/com/practice/algorithm/math/RectangleArea.java
@@ -0,0 +1,68 @@
+package src.com.practice.algorithm.math;
+
+/*
+Given the coordinates of two rectilinear rectangles in a 2D plane, return the total
+area covered by the two rectangles.
+
+The first rectangle is defined by its bottom-left corner (ax1, ay1) and its
+ top-right corner (ax2, ay2).
+
+The second rectangle is defined by its bottom-left corner (bx1, by1) and its top-right
+ corner (bx2, by2).
+
+
+
+Example 1:
+
+Rectangle Area
+Input: ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2
+Output: 45
+Example 2:
+
+Input: ax1 = -2, ay1 = -2, ax2 = 2, ay2 = 2, bx1 = -2, by1 = -2, bx2 = 2, by2 = 2
+Output: 16
+
+
+Constraints:
+
+-104 <= ax1 <= ax2 <= 104
+-104 <= ay1 <= ay2 <= 104
+-104 <= bx1 <= bx2 <= 104
+-104 <= by1 <= by2 <= 104
+* */
+public class RectangleArea {
+
+    public static void main(String[] args) {
+        RectangleArea ra = new RectangleArea();
+        int area = ra.computeArea(-3, 0, 3, 4, 0, -1, 9, 2);
+        System.out.println("Area :" + area);
+    }
+
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+
+        int ah = ay2 - ay1;
+        int al = ax2 - ax1;
+        int aA = ah * al;
+
+        int bh = by2 - by1;
+        int bl = bx2 - bx1;
+        int bA = bh * bl;
+
+        //no overlap rectangle
+        if (ax2 <= bx1 || bx2 <= ax1 || ay2 <= by1 || by2 <= ay1) {
+            return aA + bA;
+        }
+
+        //find the overlapping rectangle points
+        int overlapLeft = Math.max(ax1, bx1);
+        int overlapRight = Math.min(ax2, bx2);
+        int overlapTop = Math.max(ay1, by1);
+        int overlapBottom = Math.min(ay2, by2);
+
+        int oh = overlapRight - overlapLeft;
+        int ol = overlapBottom - overlapTop;
+        int oA = oh * ol;
+        return aA + bA - oA;
+    }
+
+}
